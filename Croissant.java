c5182c80c67696c34abbebc9f0dc0901c2fb939e
lemuel1604 <duran_lemuel@plpasig.edu.ph>
2025-05-18 22:02:57 +0800

    git commit -m "first branch save"

--- Croissant.java
+++ Croissant.java
@@ -2,6 +2,6 @@ package caseStudy;
 
 public class Croissant  extends Products{
 	public Croissant(double price, int stock, String ID_product) {
-		super("Croissant", price, stock, ID_product);
+		super("Croiiiissant", price, stock, ID_product);
 	}
 }
