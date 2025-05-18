c2f25a50038dcf447f9526bf084784bb3ce4d7fc
lemuel1604 <duran_lemuel@plpasig.edu.ph>
2025-05-18 23:04:52 +0800

    "back"

--- Cheesecake.java
+++ Cheesecake.java
@@ -3,8 +3,7 @@ package caseStudy;
 public class Cheesecake extends Products{
 	public Cheesecake(double price, int stock, String ID_product) {
 		super("Cheese Cake", price, stock, ID_product);
-		//dsdsdsdsddsdsds
-		
+
 	}
 }
 
